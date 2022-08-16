4910cebfd199e0d13ad453f0839affebcacb4daf
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2022-08-16 23:02:32 +0000

    added sample image file to directory, updated automation functionalities

--- main.py
+++ main.py
@@ -1,46 +1,62 @@
 import os
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import *
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as condition
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 
 url = "https://www.techlistic.com/p/selenium-practice-form.html"
-image_path = r"C:\Users\LordPhoenix\Pictures\js.png"
+image_path = os.path.join(os.getcwd(), 'js.png')
 os.environ['PATH'] += r"C:\selenium-chrome-drivers\chromedriver_win32"
+downloads_directory = os.path.join(os.getcwd(), "test_downloads")
+options = webdriver.ChromeOptions()
+download_preference = {"download.default_directory": downloads_directory}
+driver_wait_in_seconds = 10
+options.add_experimental_option("prefs", download_preference)
 
-driver = webdriver.Chrome()
-driver.get(url)
+firstname = "John"
+lastname = "Doe"
+driver = webdriver.Chrome(options=options)
+# open url and maximize window
+try:
+    driver.get(url)
+    driver.maximize_window()
+except Exception as e:
+    print(e)
+    driver.quit()
 
 # assert 'Demo Sign-Up Selenium Automation Practice Form' in driver.page_source
 try:
-    popup = WebDriverWait(driver, 15).until(
-        EC.presence_of_element_located((By.ID, "ez-accept-all"))
+    popup = WebDriverWait(driver, driver_wait_in_seconds).until(
+        condition.presence_of_element_located((By.ID, "ez-accept-all"))
     ).click()
 
-    cookie = WebDriverWait(driver, 15).until(
-        EC.presence_of_element_located((By.ID, 'cookieChoiceDismiss'))
+    cookie = WebDriverWait(driver, driver_wait_in_seconds).until(
+        condition.presence_of_element_located((By.ID, 'cookieChoiceDismiss'))
     ).click()
 except NoSuchElementException as e:
     print(e)
-    pass
+    driver.quit()
 
 # firstname input field
-inp_field1 = WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.NAME, 'firstname'))
-)
-inp_field1.clear()
-inp_field1.send_keys('Samuel', Keys.RETURN)
+try:
+    inp_field1 = WebDriverWait(driver, driver_wait_in_seconds).until(
+        condition.presence_of_element_located((By.NAME, 'firstname'))
+    )
+    inp_field1.clear()
+    inp_field1.send_keys(firstname, Keys.RETURN)
+except NoSuchElementException as e:
+    print(e)
 
 # lastname input field
-inp_field2 = WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.NAME, 'lastname'))
+inp_field2 = WebDriverWait(driver, driver_wait_in_seconds).until(
+    condition.presence_of_element_located((By.NAME, 'lastname'))
 )
 inp_field2.clear()
-inp_field2.send_keys('Nyamekesse', Keys.RETURN)
+inp_field2.send_keys(lastname, Keys.RETURN)
 
 # find and select the gender male
 driver.find_element(By.ID, 'sex-0').click()
@@ -67,7 +83,7 @@ drop_1 = Select(drop_down_menu_1)
 for i in drop_1.options:
     if i.text == 'Australia':
         i.click()
-        break
+
 # get commands dropdown
 drop_down_menu_2 = driver.find_element(By.ID, 'selenium_commands')
 # selecting dropdown
@@ -80,5 +96,14 @@ try:
     image_upload = driver.find_element(By.NAME, 'photo')
     image_upload.send_keys(image_path)
 
-except:
-    print("something went wrong")
+except Exception as e:
+    print(e)
+    driver.quit()
+
+try:
+    download_file = driver.find_element(By.XPATH,
+                                        "//*[@id=\"post-body-3077692503353518311\"]/div[1]/div/div/h2[2]/div[1]/div/div[28]/div[2]/div/a")
+    download_file.click()
+except NoSuchElementException as e:
+    print(e)
+    driver.quit()
